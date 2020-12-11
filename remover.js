7f57d8c51b4528f29f47a121bd856200e3500afa
mytja <52399966+mytja@users.noreply.github.com>
2020-12-11 09:02:26 +0100

    Update remover.js

--- remover.js
+++ remover.js
@@ -1,8 +1,7 @@
 // Here is our remover script
 
-function removeLogin(){
-  var element = document.querySelector('ytd-popup-container.style-scope ytd-app');
-  element.parentElement.removeChild(element);
+var element = document.querySelector('ytd-popup-container.style-scope ytd-app');
+element.parentElement.removeChild(element);
 
 function removeToS(){
   // Here will be it!
