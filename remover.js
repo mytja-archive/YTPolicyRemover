08b78855481548bcb5f71649e5fefd5f797fd16d
mytja <52399966+mytja@users.noreply.github.com>
2020-12-11 09:47:35 +0100

    Update remover.js

--- remover.js
+++ remover.js
@@ -8,6 +8,7 @@ function removeLogin(){
   try {
     var element = document.querySelector('ytd-popup-container');
     element.remove();
+    console.log("Popup container removed succesfully!");
   }
   catch {
     console.log("Popup container not found!");
@@ -18,6 +19,7 @@ function removeToS(){
   try {
     var element = document.querySelector('ytd-consent-bump-lightbox');
     element.remove();
+    console.log("ToS container removed succesfully!");
   }
   catch {
     console.log("ToS container not found!");
